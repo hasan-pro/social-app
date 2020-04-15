461185942d09a9b899915432aff2c22ea10f0750
hasan-pro <smmahady52@gmail.com>
2020-04-15 07:30:38 +0600

    post model part 1

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,14 @@
+const Post = require('../model/Post')
+
 exports.viewCreateScreen = function(req, res) {
     res.render('create-post')
+}
+
+exports.create = function(req, res) {
+    let post = new Post(req.body)
+    post.create().then(() => {
+        res.send('Created post success.')
+    }).catch((errors) => {
+        res.send(errors)
+    })
 }
